3872a397f4f135d903e4244af3c5247009aca7fd
Ahmed <ahmedisaevdev@gmail.com>
2022-10-13 04:04:18 +0100

    2_HM

--- 1l_AkhmedIsaev.playground/Contents.swift
+++ 1l_AkhmedIsaev.playground/Contents.swift
@@ -12,4 +12,17 @@ var b_pow = pow(b, 2)
 
 var D = b_pow - 4 * a * c
 
-print (D)
+var x = -1 * b / 2 * a
+var x1 = -1 * b + sqrt(D) / 2 * a
+var x2 = -1 * b - sqrt(D) / 2 * a
+
+if D > 0 {
+    
+    print("Уравнение имеет два корня x1 =" + x1 + "x2 =" + x2)
+}
+else if D < 0 {
+    print("Нет корней")
+}
+else D == 0 {
+    print("Уравнение имеет один корень x =" + x)
+}
