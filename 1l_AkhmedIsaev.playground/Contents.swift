4f6c1c4a0b2f495d1f69d0cf543d2931555c9455
Ahmed <ahmedisaevdev@gmail.com>
2022-10-13 14:40:02 +0100

    3_HM

--- 1l_AkhmedIsaev.playground/Contents.swift
+++ 1l_AkhmedIsaev.playground/Contents.swift
@@ -2,11 +2,11 @@ import UIKit
 
 // 1. Решить квадратное уравнение.
 
-// ax^2 + bx + c = 0
+// x^2 - 70x + 600 = 0
 
-var a: Double = 3
-var b: Double = 7
-var c: Double = 5
+var a: Double = 1
+var b: Double = -70
+var c: Double = 600
 
 var b_pow: Double = pow(b, 2)
 
@@ -18,11 +18,36 @@ var x2: Double = -1 * b - sqrt(D) / 2 * a
 
 if D > 0 {
     
-    print("Уравнение имеет два корня x1 = \(x1) и x2 = \(x2)")
+    print("Задача №1: Уравнение имеет два корня x1 = \(x1) и x2 = \(x2)")
 }
 else if D < 0 {
-    print("Нет корней")
+    print("Задача №1: Нет корней")
 }
 else if D == 0 {
-    print("Уравнение имеет один корень x = \(x)")
+    print("Задача №1: Уравнение имеет один корень x = \(x)")
 }
+
+// 2. Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
+
+var leg_1: Double = 4
+var leg_2: Double = 5
+var pow_leg_1 = pow(leg_1, 2)
+var pow_leg_2 = pow(leg_2, 2)
+
+var pow_hypotenuse = pow_leg_1 + pow_leg_2
+var sqrt_hypoyenuse = sqrt(pow_hypotenuse)
+
+var area_of_triangle = 0.5 * leg_1 * leg_2
+
+print("Задача №2 Площадь прямоугольного треугольника равна \(area_of_triangle)")
+
+
+//3. * Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет.
+
+var summ_vklad: Double = 10000
+var percent_per_year: Double = 7
+var years: Double = 10
+
+var result = ((summ_vklad * percent_per_year * years) / 100) + summ_vklad
+
+print(result)
