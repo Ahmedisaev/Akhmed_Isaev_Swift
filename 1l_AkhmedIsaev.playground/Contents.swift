16300beea00b6de76ac03ece8adf7b76af3e2e2b
Ahmed <ahmedisaevdev@gmail.com>
2022-10-13 03:55:40 +0100

    1_HM

--- 1l_AkhmedIsaev.playground/Contents.swift
+++ 1l_AkhmedIsaev.playground/Contents.swift
@@ -1,2 +1,15 @@
 import UIKit
 
+// 1. Решить квадратное уравнение.
+
+// ax^2 + bx + c = 0
+
+var a: Double = 3
+var b: Double = 7
+var c: Double = 5
+
+var b_pow = pow(b, 2)
+
+var D = b_pow - 4 * a * c
+
+print (D)
