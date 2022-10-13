cc2720d59ebda391bbb6f63058001436826243fb
Ahmed <ahmedisaevdev@gmail.com>
2022-10-13 04:08:02 +0100

    H3

--- 1l_AkhmedIsaev.playground/Contents.swift
+++ 1l_AkhmedIsaev.playground/Contents.swift
@@ -8,21 +8,21 @@ var a: Double = 3
 var b: Double = 7
 var c: Double = 5
 
-var b_pow = pow(b, 2)
+var b_pow: Double = pow(b, 2)
 
-var D = b_pow - 4 * a * c
+var D: Double = b_pow - 4 * a * c
 
-var x = -1 * b / 2 * a
-var x1 = -1 * b + sqrt(D) / 2 * a
-var x2 = -1 * b - sqrt(D) / 2 * a
+var x: Double = -1 * b / 2 * a
+var x1: Double = -1 * b + sqrt(D) / 2 * a
+var x2: Double = -1 * b - sqrt(D) / 2 * a
 
 if D > 0 {
     
-    print("Уравнение имеет два корня x1 =" + x1 + "x2 =" + x2)
+    print("Уравнение имеет два корня x1 = \(x1) и x2 = \(x2)")
 }
 else if D < 0 {
     print("Нет корней")
 }
-else D == 0 {
-    print("Уравнение имеет один корень x =" + x)
+else if D == 0 {
+    print("Уравнение имеет один корень x = \(x)")
 }
